df77d5d0c3dd4bac656320ef3d2ea6a00456ee8d
Fabián Souto <fab.souto@gmail.com>
2018-04-23 12:24:50 -0300

    Add CORS to app to allow reqs from the frontend.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const router = require('./routes')
 const {format} = require('./controllers/utils')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(format.options)
